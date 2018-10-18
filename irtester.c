e210e8f377f5dea899c02965ad718ee02658f0ae
kevincao1119 <43764834+kevincao1119@users.noreply.github.com>
2018-10-18 15:20:31 -0400

    Update irtester.c

--- irtester.c
+++ irtester.c
@@ -6,28 +6,23 @@ int main(int argc, char *argv[])
   int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
-  pinMode(1,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
   while(1) {
     printf("Waiting for reset\n");
-    while(digitalRead(0) == 1){
-    printf("Got a 1\n");
-    digitalWrite(2,HIGH);
-    digitalWrite(1,LOW);
-    ifttt("http:red.eecs.yorku.ca:8080/trigger/event/with/key/211532710","kevin22")
-
-   }
-printf("waitting for events\n");
-if(digitalRead(0)==0){
+    
     while(digitalRead(0) == 0){
-    printf("Got a 0\n");
-digitalWrite(2,LOW);
-digitalWrite(1,HIGH);delay(200);
-digitalWrite(1,LOW);}
-printf("Alarm\n");
-
+    printf("Waiting for event\n");
+    digitalWrite(1, HIGH); delay(500);
+    digitalWrite(2, LOW);
+	}
+    
+    while(digitalRead(0) == 1){
+    printf("Alarm\n");
+    digitalWrite (2, HIGH); delay(500);
+    digitalWrite (1, LOW);
+  	}
   }
   /*NOTREACHED*/
   return 0 ;
-}
 }
